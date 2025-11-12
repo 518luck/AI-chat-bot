e5ebc0b0fa038ba50b77107f655394acd663fe69
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-11-12 23:46:58 +0800

    feat(editor): 集成ProseMirror实现富文本编辑器功能
    添加ProseMirror相关依赖包并实现基础编辑器功能
    移除原有contentEditable实现，改为使用ProseMirror API
    更新全局样式以适配编辑器组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import "./global.css";
+import { useRef, useEffect } from "react";
 import { ArrowUp } from "lucide-react";
+import { EditorState } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+import { schema } from "prosemirror-schema-basic";
+import { DOMParser } from "prosemirror-model";
+
+import "./global.css";
 
 function App() {
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    if (!editorRef.current) return;
+
+    // 1.初始化
+    const state = EditorState.create({
+      schema,
+      doc: DOMParser.fromSchema(schema).parse(editorRef.current),
+    });
+    // 2. 渲染
+    const view = new EditorView(editorRef.current, {
+      state,
+    });
+
+    return () => {
+      view.destroy();
+    };
+  }, []);
+
   return (
     <main className="flex h-screen flex-col items-center justify-center">
       {/* 标题 */}
@@ -15,15 +41,20 @@ function App() {
         <div>您好</div>
         <div>很高兴见到你</div>
       </section> */}
+      {/* 测试  ProseMirror */}
       {/* 底部输入框 */}
       <section className="bg-background flex w-full max-w-160 rounded-[28px] border px-2 shadow-sm">
         <div className="relative flex max-h-66.5 min-h-[56px] w-full items-center">
           <div
+            ref={editorRef}
+            className="max-h-66.5 w-full overflow-x-hidden px-4 py-2 pr-12 outline-none"
+          />
+          {/* <div
             contentEditable
             suppressContentEditableWarning
             className="max-h-66.5 w-full overflow-x-hidden overflow-y-auto px-4 py-2 pr-12 wrap-break-word whitespace-pre-wrap outline-none"
             data-placeholder="询问任何问题"
-          />
+          /> */}
           <div className="absolute right-2 bottom-2 cursor-pointer rounded-full bg-black p-1.5">
             <ArrowUp color="#fff" />
           </div>
