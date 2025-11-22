b8535c663fdd330e956edf3bed0978014391363b
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-11-22 17:40:08 +0800

    feat(消息组件): 添加Markdown渲染和代码高亮支持
    引入react-markdown及相关插件实现消息内容的Markdown渲染，添加代码高亮功能并根据主题切换样式

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,7 +1,25 @@
 import cs from "classnames";
 import type { ChatMessage } from "@/types/chatMessage-type";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
+import useThemeStore from "@/stores/theme.stores";
+import { useEffect } from "react";
 
 const Message = ({ reply }: { reply: ChatMessage[] }) => {
+  const { theme } = useThemeStore((s) => s);
+
+  useEffect(() => {
+    const load = async () => {
+      if (theme === "dark") {
+        await import("highlight.js/styles/github-dark.css");
+      } else {
+        await import("highlight.js/styles/github.css");
+      }
+    };
+    load();
+  }, [theme]);
+
   return (
     <section>
       {reply.length !== 0 && (
@@ -15,14 +33,19 @@ const Message = ({ reply }: { reply: ChatMessage[] }) => {
             >
               <div
                 className={cs(
-                  "mb-9 inline-flex min-h-9 w-fit flex-col rounded-2xl p-4",
+                  "prose prose-sm dark:prose-invert s crollbar-thin mb-9 inline-flex min-h-9 w-fit flex-col overflow-x-auto rounded-2xl p-4 wrap-break-word",
                   {
                     "max-w-[448px] bg-[#f4f4f4] dark:bg-[#303030]":
                       item.type === "user",
                   },
                 )}
               >
-                {item.payload.content}
+                <ReactMarkdown
+                  remarkPlugins={[remarkGfm]}
+                  rehypePlugins={[rehypeHighlight]}
+                >
+                  {item?.payload?.content}
+                </ReactMarkdown>
               </div>
             </div>
           ))}
